52b682dcbac2068ac95aa61a6d96476dfd59333e
hansaja-gunasekara <hgmgunasekara@gmail.com>
2024-01-06 19:46:09 +0530

    update: change mongodb url

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const app = express();
 
 // MongoDB connection
 mongoose
-  .connect("mongodb+srv://admin:admin@admin.vkhydjp.mongodb.net/", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.hcjjnxy.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
